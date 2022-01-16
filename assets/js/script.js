5290f58429721fa99dedf76672cb526fa5761dc7
adamvaughn <adamdvaughn@gmail.com>
2022-01-15 19:02:37 -0600

    added questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,32 @@
 var time = 75;
 
 // Quiz Questions
-var start = false;
+var questions = [
+    { q: "Commonly used data types DO NOT include:",
+      a: "ANSWER",
+      choices: [{choice: "strings"}, {choice: "booleans"}, {choice: "alerts"}, {choice: "numbers"}]
+    },
+
+    { q: "The condition in an if/else statement is enclosed with ______.",
+      a: "ANSWER",
+      choices: [{choice: "quotes"}, {choce: "curly brackets"}, {choice: "parenthesis"}, {choice: "square brackets"}]
+    },
+
+    { q: "Arrays in JavaScript can be used to store ______.",
+      a: "ANSWER",
+      choices: [{choice: "numbers and strings"}, {choice: "other arrays"}, {choice: "booleans"}, {choice: "all of the above"}]
+    },
+
+    { q: "String values must be enclosed in ______ when being assigned to variables",
+      a: "ANSWER",
+      choices: [{choice: "commas"}, {choice: "curly brackets"}, {choice: "quotes"}, {choice: "parenthesis"}]
+    },
+
+    { q: "A very useful tool used during development and for printing content to the debugger is:",
+      a: "console.log",
+      choices: [{choice: "JavaScript"}, {choice: "terminal/basg"}, {choice: "for loops"}, {choice: "console.log"}]
+    }
+]
 
 // Final score/initial log
 
